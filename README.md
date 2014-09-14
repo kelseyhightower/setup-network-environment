8df30f9b8a684dba04b938f827433d928f3253b6
Kelsey Hightower <kelsey.hightower@gmail.com>
2014-09-14 11:46:48 -0700

    add setup-network-environment

---
+++ README.md
@@ -0,0 +1,72 @@
+# setup-network-environment
+
+## Usage
+
+```
+setup-network-environment
+```
+
+The network info will be written to `/etc/network-environment` by default.
+
+```
+LO_IPV4=127.0.0.1
+ENO16777736_IPV4=192.168.12.10
+DEFAULT_IPV4=192.168.12.10
+```
+
+You can write the network info to a different file using the `-o` flag.
+
+```
+setup-network-environment -o network-environment
+```
+
+### systemd Integration
+
+
+#### setup-network-environment.service
+
+```
+[Unit]
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+ExecStartPre=-/usr/bin/mkdir -p /opt/bin
+ExecStartPre=/usr/bin/wget -N -P /opt/bin https://storage.googleapis.com/sne/setup-network-environment
+ExecStartPre=/usr/bin/chmod +x /opt/bin/setup-network-environment
+ExecStart=/opt/bin/setup-network-environment
+```
+
+#### Depending on the setup-network-environment.service
+
+```
+[Unit]
+Requires=setup-network-environment.service
+After=setup-network-environment.service
+
+[Service]
+EnvironmentFile=/etc/network-environment
+ExecStart=/opt/bin/kubelet --hostname_override=${DEFAULT_IPV4}
+```
+
+The above unit file will override the kubelet hostname with the IP address of the interface that provides the default gateway. You can also reference a specific interface:
+
+```
+ExecStart=/opt/bin/kubelet --hostname_override=${ENO16777736_IPV4}
+```
+
+## Building
+
+```
+mkdir -p "${GOPATH}/src/github.com/kelseyhightower"
+cd "${GOPATH}/src/github.com/kelseyhightower"
+git clone https://github.com/kelseyhightower/setup-network-environment.git
+cd setup-network-environment
+godep go build .
+```
+
+Slightly smaller binary.
+
+```
+CGO_ENABLED=0 GOOS=linux godep go build -a -tags netgo -ldflags '-w' .
+```
---
+++ main.go
@@ -0,0 +1,93 @@
+// Copyright (c) 2014 Kelsey Hightower. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/docker/libcontainer/netlink"
+)
+
+var (
+	defaultEnvironmentFilePath = "/etc/network-environment"
+	environmentFilePath        string
+)
+
+func init() {
+	log.SetFlags(0)
+	flag.StringVar(&environmentFilePath, "o", defaultEnvironmentFilePath, "environment file")
+}
+
+func main() {
+	flag.Parse()
+	tempFilePath := environmentFilePath + ".tmp"
+	tempFile, err := os.Create(tempFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tempFile.Close()
+	if err := writeEnvironment(tempFile); err != nil {
+		log.Fatal(err)
+	}
+	os.Rename(tempFilePath, environmentFilePath)
+}
+
+func writeEnvironment(w io.Writer) error {
+	var buffer bytes.Buffer
+	defaultIfaceName, err := getDefaultGatewayIfaceName()
+	if err != nil {
+		// A default route is not required; log it and keep going.
+		log.Println(err)
+	}
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return err
+	}
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return err
+		}
+		for _, addr := range addrs {
+			ip, _, err := net.ParseCIDR(addr.String())
+			// Record IPv4 network settings. Stop at the frist IPv4 address
+			// found for the interface.
+			if err == nil && ip.To4() != nil {
+				buffer.WriteString(fmt.Sprintf("%s_IPV4=%s\n", strings.ToUpper(iface.Name), ip.String()))
+				if defaultIfaceName == iface.Name {
+					buffer.WriteString(fmt.Sprintf("DEFAULT_IPV4=%s\n", ip.String()))
+				}
+				break
+			}
+		}
+	}
+	if _, err := buffer.WriteTo(w); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getDefaultGatewayIfaceName() (string, error) {
+	routes, err := netlink.NetworkGetRoutes()
+	if err != nil {
+		return "", err
+	}
+	for _, route := range routes {
+		if route.Default {
+			if route.Iface == nil {
+				return "", errors.New("found default route but could not determine interface")
+			}
+			return route.Iface.Name, nil
+		}
+	}
+	return "", errors.New("unable to find default route")
+}
